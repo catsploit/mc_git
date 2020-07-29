a5e56d0de86bf800f8280cf5894e310854585ba3
catsploit <nicolas.schnitz@gmail.com>
2020-07-29 19:27:54 -0400

    modified getenv output

--- config/tools/getenv.py
+++ config/tools/getenv.py
@@ -3,7 +3,7 @@
 
 import nmap
 import time
-from config.functions import is_root, cprint
+from config.functions import is_root
 from config.tools.host_geo import geolocate_host
 from config.colors import *
 
@@ -19,7 +19,7 @@ def getEnvironment(parameters):
     if is_root() != True:
         raise RootRequired("instruction require root priviliges for -sV & -O flags")
 
-    cprint("[~] getenv >> Scanning target . . .", 'cyan', True)
+    print("{0}{1}[~] {0}getenv >> {1}Scanning target . . .".format(cyan, bold))
     Scanner = nmap.PortScanner()
     result = Scanner.scan(str(ip), '25500-25565', '-sV -O\n' )
 
@@ -38,20 +38,21 @@ def getEnvironment(parameters):
             cpe = OS_LIST[match]['osclass'][0]['cpe'][0].strip('cpe:/o:')
             break
 
-    os_string = f"""
-    I>>>>>>>>>>>>OS_MATCH RESULTS<<<<<<<<<<<<<I
-    OS => {osname}
-    CPE => {cpe}
-    CHANCES => {OS_CHOOSEN}%
-    I>>>>>>>>>>>>----------------<<<<<<<<<<<<<I\n"""
+    os_string = """{5}
+---------OS_MATCH---------
+{3}{4}[-] {5}OS : {6}{0}
+{3}{4}[-] {5}CPE : {6}{1}
+{3}{4}[-] {5}CHANCES : {6}{2}{5}%\n""".format(osname, cpe, OS_CHOOSEN, blue, bold, normal, red)
 
-    cprint("[+] getenv >> MATCH! OS-scan results:", 'green', True)
-    print(f"{red}{bold}{os_string}")
+    # - Printing OS MATCH RESULTS - #
+    print("{0}{1}[+] {0}getenv >> {1}MATCH!{0} OS-scan results:".format(green, bold))
+    print(f"{os_string}")
 
     # - geolocate host by ip-api - #
-    cprint("[~] getenv >> geolocating target . . .", 'cyan', True)
+    print("---------GEOLOCATE_RESULTS---------")
+    print("{0}{1}[~] {0}getenv >> {1}geolocating target . . .".format(cyan, bold))
     geolocate_result = geolocate_host(ip)
-    cprint("[+] getenv >> Located\n", 'green', True)
+    print("{0}{1}[+] {0}getenv >> {1}Located{0} - printing results\n".format(green, bold))
     print(geolocate_result)
 
     if flag == '-out':
